ee333aa1ffb6d8e616347e5980aecf4fa81aed45
padoura <>
2017-11-13 11:01:54 +0200

    - Added FindLastChar: A simple method to find the last occurrence of a character in a String by using a do-while statement.

---
+++ SimpleExamples/src/lesson6/FindLastChar.java
@@ -0,0 +1,26 @@
+package lesson6;
+
+/**
+ *
+ * @author padou
+ */
+public class FindLastChar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StringBuilder buffer2 = new StringBuilder("10hel10lo10");
+        System.out.println(findLastChar(buffer2, 'l')); 
+    }
+    
+    private static int findLastChar(StringBuilder buffer, char c){
+        int next  = 0;
+        int current=0;
+        do{
+            current = next;
+            next = buffer.toString().indexOf(c, next+1);
+        }while(next != -1);
+        return current;
+    }
+}
